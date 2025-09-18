b0881b3467359e062ce4f7f638109f25ad331d91
Stiven <2021110357@udh.edu.pe>
2025-09-17 22:31:07 -0500

    .

--- database/migrations/2025_09_17_024651_create_roles_table.php
+++ database/migrations/2025_09_17_024651_create_roles_table.php
@@ -3,24 +3,26 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre', 50)->unique();       // Nombre del rol (Pasajero, Conductor, Administrador)
-            $table->timestamps(); 
+            $table->string('nombre', 50)->unique(); // Administrador, Pasajero, Conductor
+            $table->timestamps();
         });
+
+        // Insertar roles fijos
+        DB::table('roles')->insert([
+            ['nombre' => 'Administrador', 'created_at' => now(), 'updated_at' => now()],
+            ['nombre' => 'Pasajero', 'created_at' => now(), 'updated_at' => now()],
+            ['nombre' => 'Conductor', 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('roles');
